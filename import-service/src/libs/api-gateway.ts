05e42b6428018e2f63d038f8a01ba6aec25d4306
ambercode <andrius.urbelis@gmail.com>
2022-08-08 13:58:44 +0300

    Task5: Integration with S3 (#3)
    * task3: products
    
    * task3: get product by Id
    
    * task4: get products from DB
    
    * task4: product by id
    
    * task4: client per request
    
    * task4: fix by id
    
    * task4: create product
    
    * task4: validation
    
    * task5: init
    
    * task5: upload product
    
    * task5: parse
    
    Co-authored-by: Andrius Urbelis <andrius.urbelis@coxautoinc.com>

---
+++ import-service/src/libs/api-gateway.ts
@@ -0,0 +1,16 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import type { FromSchema } from "json-schema-to-ts";
+
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+
+export const formatJSONResponse = (statusCode: number, response: any) => {
+  return {
+    statusCode: statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*'
+    },
+    body: JSON.stringify(response)
+  }
+}
